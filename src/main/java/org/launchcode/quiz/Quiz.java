8a3c02aa18ef3603c1aab33aa5c90e43d4845a23
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-04-13 20:28:01 -0500

    finished the basic section of the studio.  didnt get to bonuses yet

--- src/main/java/org/launchcode/quiz/Quiz.java
+++ src/main/java/org/launchcode/quiz/Quiz.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Quiz {
-
+    Integer userScore = 0;
     ArrayList<Question> quizQuestions = new ArrayList<>();
+
+    ArrayList<Question> missedQuestions = new ArrayList<>();
 
     public void addQuestion(Question aQuestion){
         quizQuestions.add(aQuestion);
@@ -19,21 +21,53 @@ public class Quiz {
         for (Question question : quizQuestions) {
             question.printQuestion();
             System.out.print("Enter your answer: ");
-            question.userAnswer = scanny.nextLine().toUpperCase();
+            question.userAnswer = scanny.nextLine().toUpperCase().replaceAll("[^A-Z]", "");
             System.out.println("");
 
         }
-        System.out.println(gradeQuiz());
+        gradeQuiz();
+        System.out.println("Your score was " + userScore + " out of " + quizQuestions.size());
+        if (userScore == quizQuestions.size()){
+            System.out.println("Awesome you got all the answers correct!");
+        } else {
+            System.out.println("You missed the following question(s):");
+            for (Question missedQuestion : missedQuestions ) {
+                missedQuestion.printQuestion();
+                System.out.print("The correct answer is: ");
+                for (String correctAns : missedQuestion.correctAnswer){
+                    System.out.print(correctAns + " ");
+                }
+                System.out.println("");
+            }
+        }
 
     }
 
-    public int gradeQuiz(){
-        int score = 0;
+    public void gradeQuiz(){
         for (Question question : quizQuestions) {
-            if(question.correctAnswer.equals(question.userAnswer)){
-                score++;
+
+            if (question.correctAnswer.size() == 1) {
+                if (question.correctAnswer.contains(question.userAnswer)){
+                    userScore++;
+                } else {
+                    missedQuestions.add(question);
+                }
+
+            } else {
+                Integer tempCorrectAnswer = 0;
+                String[] userAnswerArray = question.userAnswer.split("");
+                for (String userAnswer : userAnswerArray){
+                    if(question.correctAnswer.contains(userAnswer)){
+                        tempCorrectAnswer++;
+                    }
+                }
+                if (tempCorrectAnswer == question.correctAnswer.size()){
+                    userScore++;
+                } else {
+                    missedQuestions.add(question);
+                }
+
             }
         }
-        return score;
     }
 }
